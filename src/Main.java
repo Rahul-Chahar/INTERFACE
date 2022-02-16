156df2706f49fb8fad8b85688321ecb96b1e460a
Rahul-Chahar <rahul.kumar_cs19@gla.ac.in>
2022-02-17 00:00:16 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/*
+ *Author Name: Rahul Chahar
+ *IDE: IntelliJ IDEA Community Edition
+ *Date: 16/02/2022
+ */
+
+public class Main {
+}
---
+++ src/TEST.java
@@ -0,0 +1,26 @@
+/*
+ *Author Name: Rahul Chahar
+ *IDE: IntelliJ IDEA Community Edition
+ *Date: 16/02/2022
+ */
+
+ interface TEST {
+    void meth1();
+    void meth2();
+}
+class My extends TEST
+{
+    public void meth1();
+}
+
+    public static void main(String[] args) {
+     public static void main();
+        System.out.println("asdf");
+        }
+    }
+    public class interfaceprakash;
+{
+    hijab (String args);
+
+
+        }
